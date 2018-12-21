ebb9c46a40b59d7fcae9766aab4d40adce4de741
UMULISA12 <email@example.com>
2018-12-21 16:28:51 +0200

    creating function which check if all questions are answered

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function noAnswer(){
+        
+    if (!$('[name=Q1]').is(':checked') || !$('[name=Q2]').is(':checked') || !$('[name=Q3]').is(':checked') || !$('[name=Q4]').is(':checked') || !$('[name=Q5]').is(':checked') || !$('[name=Q6]').is(':checked') || !$('[name=Q7]').is(':checked') || !$('[name=Q8]').is(':checked') || !$('[name=Q9]').is(':checked')){
+
+        return true;
+
+    }       
+
+}
